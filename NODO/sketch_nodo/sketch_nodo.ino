e8ae0aa8e5de85d661bb63a7ef79a9c30ab0a8a4
Manuel Domínguez <mandomvaz@gmail.com>
2015-04-09 19:13:58 +0200

    Añadido control PIR
    Tambien comentado de codigo y mejorada la coherencia del nombramiento de
    variables

--- NODO/sketch_nodo/sketch_nodo.ino
+++ NODO/sketch_nodo/sketch_nodo.ino
@@ -1,35 +1,90 @@
-/* CONFIGURACION */
-//MY del nodo
-char my[] = "01";
-//Numero de interruptores y sus pines correspondientes.
+
+///////////////////
+//               //
+// CONFIGURACION //
+//               //
+///////////////////
+
+
+/////////////////
+// MY del nodo //
+/////////////////
+
+const char MY[] = "01";
+
+
+//////////////////////////////////////////////////////////
+//Numero de interruptores y sus pines correspondientes. //
+//////////////////////////////////////////////////////////
+
+
 #define NUM_INT 1
-int pinIntp[] = {8};
-//Numero de actuadores y sus pines correspondientes.
-//Para cada interruptor tiene que haber un actuador. Si dos interruptores
-//actuan sobre el mismo actuador, duplicar el actuador en el vector.
+int PIN_INTERRUPTORES[] = {8};
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Numero de actuadores y sus pines correspondientes.                      //
+// Para cada interruptor tiene que haber un actuador. Si dos interruptores //
+// actuan sobre el mismo actuador, duplicar el actuador en el vector.      //
+/////////////////////////////////////////////////////////////////////////////
+
 #define NUM_ACT 1
-int pinAct[] = {13};
-//Pin del sensor de temperatura. El sensor utilizado aqui es el lm35, este pin debe ser analogico.
-int pinTemp = 1;
-int vector_temperatura[10];
-int bucle_temp = 0;
+int PIN_ACTUADORES[] = {13};
 
-/* VARIABLES GLOBALES */
-//Vector de estado de interruptores
-int estadoIntp[NUM_INT];
-//Vector de valores de los actuadores
-int valorAct[NUM_ACT];
-//Variables de control de mensajes
-int func, act, val;
-int flagmsj = 0;
-//Timers
-unsigned long int_time[NUM_INT];
-//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Pin del sensor de temperatura. El sensor utilizado aqui es el lm35, este pin debe ser analogico.  //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int PIN_TEMPERATURA = 1;
+int ARRAY_TEMPERATURA[10];
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Pin del sensor PIR.                                                                           //
+// La posicion de su actuador dentro del vector de actuadores.                                   //
+// Y  el tiempo que debe estar encendido. 30000 (30s) para pasillos y 300000 (5min) para baños   //
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
+int PIN_PIR = 10;
+int ACTUADOR_PIR = 0;
+int TIEMPO_PIR = 30000;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Pin de la resistencia LDR. Este al igual que el de la temperatura tambien debe ser un pin analogico. //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int PIN_LDR = 2;
+
+/////////////////////////
+//                     //
+// VARIABLES GLOBALES  //
+//                     //
+/////////////////////////
+
+///////////////////////////////////////
+// Vector de estado de interruptores //
+///////////////////////////////////////
+
+int ESTADO_INTERRUPTORES[NUM_INT];
+
+/////////////////////////////////////////
+// Vector de valores de los actuadores //
+/////////////////////////////////////////
+
+int VALOR_ACTUADORES[NUM_ACT];
+
+//////////////////////////////////////
+// Variables de control de mensajes //
+//////////////////////////////////////
+
+int FUNCION_MSJ, ACTUADOR_MSJ, VALOR_MSJ;
+int FLAG_MSJ = 0;
+
+////////////
+// Timers //
+////////////
+
+unsigned long TIMER_INTERRUPTOR[NUM_INT];
+unsigned long TIMER_PIR;
 
 
 void setup() {
@@ -38,15 +93,15 @@ void setup() {
   
   //Interruptores, inicializacion
   for(i = 0; i < NUM_INT; i++){
-    pinMode(pinIntp[i], INPUT_PULLUP);
-    estadoIntp[i] = digitalRead(pinIntp[i]);
-    int_time[i] = 0;
+    pinMode (PIN_INTERRUPTORES[i], INPUT_PULLUP);
+    ESTADO_INTERRUPTORES[i] = digitalRead (PIN_INTERRUPTORES[i]);
+    TIMER_INTERRUPTOR[i] = 0;
   }
   
   //Actuadores, inicializacion
   for(i = 0; i < NUM_ACT; i++){
-    pinMode(pinAct[i], OUTPUT);
-    valorAct[i] == 0;
+    pinMode(PIN_ACTUADORES[i], OUTPUT);
+    VALOR_ACTUADORES[i] == 0;
   }
   
   //Sensores
@@ -56,43 +111,62 @@ void setup() {
 
 
 void loop() {
-  int i, est_aux;
- 
-  
-  /* Comunicaciones Recepcion */
+  int i, est_aux, cantidad_luz;
+  static int bucle_temp = 0;
+  static int array_luz[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  static int flag_pir = 0;
+////////////////////////////
+// RECEPCION DE MENSAJES  //
+////////////////////////////
+
   if(Serial.available() > 0){
     recibeMensaje();
-    if(flagmsj == 1){
-      flagmsj = 0;
+    if(FLAG_MSJ == 1){
+      FLAG_MSJ = 0;
       procesaMensaje();
     }
   }
   
-  /* Sensores e Interruptores */
+///////////////////////////////
+// SENSORES E INTERRUPTORES  //
+///////////////////////////////
   
   //Control de interruptores
   //se comprueban todos los pines de interruptores en busca de cambios en su estado.
   for(i = 0; i < NUM_INT; i++){
-    est_aux = digitalRead(pinIntp[i]);
-    if(est_aux != estadoIntp[i]){
-      estadoIntp[i] = est_aux;
+    est_aux = digitalRead (PIN_INTERRUPTORES[i]);
+    if(est_aux != ESTADO_INTERRUPTORES[i]){
+      ESTADO_INTERRUPTORES[i] = est_aux;
   //Cuando encontramos un cambio hay que comprobar que no se trate de un rebote en la señal.
   //Para ello guardamos el tiempo que lleva arduino encendido.
-      if(int_time[i] == 0){
-        int_time[i] = millis();
+      if(TIMER_INTERRUPTOR[i] == 0){
+        TIMER_INTERRUPTOR[i] = millis();
         
-        if(valorAct[i] == 0){
-          valorAct[i] = 1; 
+        if(VALOR_ACTUADORES[i] == 0){
+          VALOR_ACTUADORES[i] = 1; 
+
+          //Evita que el sensor de movimiento apague el actuador accidentalmente.
+          if(i == PIN_PIR){
+            flag_pir = 1;
+            TIMER_PIR = 0;
+          }
+          
           enviaMsj(10,i,1);
         }else{
-          valorAct[i] = 0;
+          VALOR_ACTUADORES[i] = 0;
+          
+          if(i == PIN_PIR){
+            flag_pir = 0;
+            TIMER_PIR = 0;
+          }
+
           enviaMsj(10,i,0);
         }
         
       }else{
-  //Cuando hayan pasado 2 segundos volvemos al estado inicial.
-        if( millis() - int_time[i] > 2000){
-          int_time[i] = 0;
+  //Cuando haya pasado 1 segundos volvemos al estado inicial.
+        if( millis() - TIMER_INTERRUPTOR[i] > 1000){
+          TIMER_INTERRUPTOR[i] = 0;
         }
       }
     }
@@ -100,41 +174,78 @@ void loop() {
   
   //Lectura de sensores
   //Temperatura
-  vector_temperatura[bucle_temp] = 100 + bucle_temp; //analogRead(pinTemp);
+  ARRAY_TEMPERATURA[bucle_temp] = 100 + bucle_temp; //analogRead(PIN_TEMPERATURA);
+  //Luz
+  array_luz[bucle_temp] = 100; //analogRead(PIN_LDR);
+
+
   bucle_temp = (bucle_temp + 1) % 10;
   
-  
-  /* Actualizacion de Actuadores */
-  for(i = 0; i < NUM_ACT; i++){
-    if(valorAct[i] == 0){
-      digitalWrite(pinAct[i], LOW);
+
+  //Control de activacion por movimiento
+  //flag_pir se usa para evitar encendidos y apagados accidentales, es decir, el interruptor tiene preferencia.
+
+  if(flag_pir == 0 && digitalRead(PIN_PIR) == HIGH){
+    
+  //Si se activa el sensor de movimiento y TIMER_PIR es 0, calculamos la cantidad de luz para saber si es necesario encender la luminaria.
+  //Si TIMER_PIR ya contiene un valor, no hay comprobar la luz, puesto que la luminaria esta encendida, nunca se apagaria.
+    if(TIMER_PIR == 0){
+      for(i = 0; i < 10; i++){
+       cantidad_luz += array_luz[i];
+      }
+      cantidad_luz = cantidad_luz / 10;
     }else{
-      digitalWrite(pinAct[i], HIGH);
+      cantidad_luz = 0;
+    }
+    
+    if(cantidad_luz < 500){
+      TIMER_PIR = millis();
+      VALOR_ACTUADORES[ACTUADOR_PIR] = 1;
+    }
+  }
+
+  //Para apagar el actuador del pir esperamos el tiempo definido en la configuracion.
+  
+  if( TIMER_PIR != 0 && (millis() - TIMER_PIR > TIEMPO_PIR)){
+      TIMER_PIR = 0;
+      VALOR_ACTUADORES[ACTUADOR_PIR] = 0;
+  }
+
+
+//////////////////////////////////////
+// ACTUALIZACION DE LOS ACTUADORES  //
+//////////////////////////////////////
+  
+  for(i = 0; i < NUM_ACT; i++){
+    if(VALOR_ACTUADORES[i] == 0){
+      digitalWrite(PIN_ACTUADORES[i], LOW);
+    }else{
+      digitalWrite(PIN_ACTUADORES[i], HIGH);
     }
   }
 }
 
 
 void recibeMensaje(){
-  char aux[3], auxval[4];
+  char aux[3];
   char msj[9] = "";
-  int p1, p2, p3, p4;
   int numbytes = 0;
   numbytes = Serial.readBytes(msj, 9);
   if(numbytes == 9){
     aux[0] = msj[0];
     aux[1] = msj[1];
     aux[2] = '\0';
-    if(strcmp(my, aux) == 0){
-      sscanf(msj, "%2d%2d%2d%3d",NULL,&func,&act,&val);
-      flagmsj = 1;
+//solo si el mensaje va dirigido a este nodo, o a la direccion de broadcast.
+    if(strcmp(MY, aux) == 0 || strcmp("00", aux) == 0){
+      sscanf(msj, "%2d%2d%2d%3d",NULL,&FUNCION_MSJ,&ACTUADOR_MSJ,&VALOR_MSJ);
+      FLAG_MSJ = 1;
     } 
   }else{
     enviaMsj(99, 0, 0);
   }
 }
 void procesaMensaje(){
-  switch(func){
+  switch(FUNCION_MSJ){
         case 0:
           apagarTodo();
           break;
@@ -153,17 +264,17 @@ void procesaMensaje(){
 
 void apagarTodo(){
  for(int i; i < NUM_ACT; i++){
-   valorAct[i] = 0;
+   VALOR_ACTUADORES[i] = 0;
  }
  enviaMsj(0,0,0);
 }
 
 void setActuador(){
-  if(act < NUM_ACT){
-    valorAct[act] = val;
-    enviaMsj(func, act, val);
+  if(ACTUADOR_MSJ < NUM_ACT){
+    VALOR_ACTUADORES[ACTUADOR_MSJ] = VALOR_MSJ;
+    enviaMsj(FUNCION_MSJ, ACTUADOR_MSJ, VALOR_MSJ);
   }else{
-    enviaMsj(99, act, val);
+    enviaMsj(99, ACTUADOR_MSJ, VALOR_MSJ);
   }
 }
 
@@ -171,7 +282,7 @@ void notificarSensor(){
   int suma = 0;
   float media;
   for(int i = 0; i < 10; i++){
-    suma = suma + vector_temperatura[i];
+    suma = suma + ARRAY_TEMPERATURA[i];
   }
   media = (float)suma / 10;
   media = (5 * media * 100) / 1024;
@@ -181,6 +292,6 @@ void notificarSensor(){
 
 void enviaMsj(int funcion, int actuador, int valor){
   char ack[10];
-  sprintf(ack, "%s%02d%02d%03d", my, funcion, actuador, valor);
+  sprintf(ack, "%s%02d%02d%03d", MY, funcion, actuador, valor);
   Serial.print(ack);  
 }
